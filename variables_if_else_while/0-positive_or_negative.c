7347d0cd69d17cf86eeb3dc1d9c0aaacc1cd2848
Denis12306 <denis.gavaud@gmail.com>
2025-10-21 12:32:28 +0000

    PrintRdNumb

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,8 +9,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	for (n < 0 ; n = 0 ; n > 0);
-		printf ("n");
-		
+	if (n > 0);
+		printf("n is positive \n");
+	else if (n < 0);
+		printf ("n is negative \n");
+	else if (n = 0);
+	        printf ("n is zero \n");
+	
 	return (0);
 }
