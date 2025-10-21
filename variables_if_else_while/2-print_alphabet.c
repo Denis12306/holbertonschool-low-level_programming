4e8b2cb1fc0602a4cf7de9e5290b89f3e9f82e48
Denis12306 <denis.gavaud@gmail.com>
2025-10-21 15:56:21 +0000

    PrintAlphab

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * Print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+	char alphabet = "a";
+{
+	while(alphabet <=z);
+	putchar (alphabet);
+
+	return (0);
+}
