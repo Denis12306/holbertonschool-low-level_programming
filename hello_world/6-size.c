fb9d51d84c8cebe612dd33c4cfbb0e22b365c7b9
Denis12306 <denis.gavaud@gmail.com>
2025-10-21 08:17:41 +0000

    SizeOf

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
         printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -13,5 +12,6 @@ int main(void)
         printf("Size of long int: %d byte(s)\n", sizeof(long int));
         printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
         printf("Size of float: %d byte(s)\n", sizeof(float));
+	
         return (0);
 }
