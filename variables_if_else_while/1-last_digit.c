15e14995e216601a25a65072488253a6a21cf101
Denis12306 <denis.gavaud@gmail.com>
2025-10-21 13:39:35 +0000

    PrintLastNumb

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,18 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	der = n % 10
+	der = n % 10;
 	if (der > 5)
 	{
-		printf("der of %d and is greater than 5\n", der);
+		printf("der of %d and is greater than 5\n", n, der);
 	}
 	else if (der = 0)
 	{
-		printf("der of %d der is 0\n", der);
+		printf("der of %d der is 0\n", n, der);
 	}
 	else (der < 6)(der ! 0)
 	{
-		printf("der of %d is %d and is less than than 6 ans not 0\n", der);
+		printf("der of %d is %d and is less than than 6 ans not 0\n", n, der);
 	}
 	return (0);
 }
