133204eba3321218487fcb04d3d1704647ed97dd
Denis12306 <denis.gavaud@gmail.com>
2025-10-21 07:55:42 +0000

    SizeOf

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 int main()
-
-        int i;
-        float f;
-        char c;
-        long int;
-        long long int;
-
-	{
-        printf("Size of a char: %lu bytes\n", sizeof(c));
-        printf("Size of an int: %lu bytes\n", sizeof(i));
-        printf("Size of long int: %lu bytes\n", sizeof(long it));
-        printf("Size of long long int: %lu bytes\n", sizeof(long long it));
-        printf("Size of float: %lu bytes\n", sizeof(f));
-
+{
+        printf("Size of a char: %lu bytes\n", sizeof(char));
+        printf("Size of an int: %lu bytes\n", sizeof(int));
+        printf("Size of long int: %lu bytes\n", sizeof(long int));
+        printf("Size of long long int: %lu bytes\n", sizeof(long long int));
+        printf("Size of float: %lu bytes\n", sizeof(float));
         return 0;
 }
