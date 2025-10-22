0d09aec77cbd886087e0f33d2a5470e52f0a9a4b
Denis12306 <denis.gavaud@gmail.com>
2025-10-22 08:26:59 +0000

    PrintAlphbExcept

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
- * main - Prints the alphabet followed by a new line
+ * main - Prints the alphabet in lowercase, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
+{
+	int main(void);
 {
 	char alphabet = 'a';
 
