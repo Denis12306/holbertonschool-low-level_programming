0f98449f81edab8a7846c46f75a74a6e8af39050
Denis12306 <denis.gavaud@gmail.com>
2025-10-22 13:23:52 +0000

    PrintBase16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int main(void);
+{
+	char number = '0';
+
+	while (number <= '16')
+	{
+		putchar(number);
+
+		number++;
+	}
+}
+
+putchar('\n');
+
+	return (0);
+}
