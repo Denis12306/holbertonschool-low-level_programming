c39561ebb23427a88ccc1aa9c8013f63757e753d
Denis12306 <denis.gavaud@gmail.com>
2025-10-22 08:39:55 +0000

    PrintNumb0-9(2)

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 0 to 9, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int main(void);
+{
+	int number = '0';
+
+	while (number <= '9')
+	{
+		putchar(number);
+
+		number++;
+	}
+}
+
+putchar('\n');
+
+	return (0);
+}
