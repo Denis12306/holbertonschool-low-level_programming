eaf801adff3187ba997b6efe04d6a799f3765e65
Denis12306 <denis.gavaud@gmail.com>
2025-10-22 08:10:19 +0000

    PrintAlphbExcept

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints the alphabet followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+
+		alphabet++;
+	}
+	if (alphabet =="q" || alphabet =="e")
+	{
+		continue;
+}
+
+putchar('\n');
+
+	return (0);
+}
