f57bcf55661d4c64348bf3251b5a7e704bff250f
Denis12306 <denis.gavaud@gmail.com>
2025-10-22 14:13:41 +0000

    PrintBase16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -6,25 +6,17 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int main(void);
-{
-	char number;
-	char alphabet;
+    char character;
+    int num;
 
-	for  (number = 0; number <= 9; number++);
-	{
-		putchar(number);
-
-	}
-	for (alphabet = 'a'; alphabet <= 'f'; alphabet++);
-
-		putchar(alphabet);
-
-}
-
-putchar('\n');
-
-	return (0);
-}
+    for (num = 0; num <= 9; num++)
+        putchar(num + '0');
+    for (character = 'a'; character <= 'f'; character++)
+    {
+        putchar(character);
+    }
+    putchar('\n');
+    return (0);
