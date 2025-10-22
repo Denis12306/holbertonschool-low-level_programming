4427614cafddc07f6a6df5ef66bd234623eb1e87
Denis12306 <denis.gavaud@gmail.com>
2025-10-22 09:24:47 +0000

    PrintREVAlphab

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int main(void);
+{
+	int alphabet = 'z';
+
+	for (alphabet >= 'a')
+	{
+		putchar(alphabet);
+
+		alphabet++;
+
+	}
+}
+
+putchar('\n');
+
+	return (0);
+}
