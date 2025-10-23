5070a6787946853448154496b4de018e9d4bded5
Denis12306 <denis.gavaud@gmail.com>
2025-10-23 12:06:34 +0000

    PrintPutchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints _putchar followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int _putchar;
+
+		printf("_putchar\n");
+	{
+	putchar(_putchar);
+	}
+	return(0);
+}
