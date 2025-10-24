e0e4e49ab63a29c3f638efca563f6984913ff40d
Denis12306 <denis.gavaud@gmail.com>
2025-10-24 12:55:29 +0000

    Print10xAplhb

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,13 @@
 void print_alphabet_x10(void)
 {
 	int j = '0';
-        int i;
-	for (; j <= '9'; j++)
+	int i;
+
+	for (j; j <= '9'; j++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 		{
-		 	_putchar(i);
+			_putchar(i);
 		}
 		_putchar('\n');
 	}
