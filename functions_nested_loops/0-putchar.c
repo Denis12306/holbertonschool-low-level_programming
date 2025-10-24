e4365652bb19a8906f7938b5eb90c9870157e781
Denis12306 <denis.gavaud@gmail.com>
2025-10-24 09:29:31 +0000

    main.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Prints _putchar followed by a new line.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints _putchar followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
         _putchar('_');
--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,19 +1,16 @@
-#include <stdio.h>
 #include "main.h"
-/**
+/*
 * main - Print the alphabet in lowercase.
 *
 * Return: Always 0 (Success)
 */
-int main(void)
+void print_alphabet(void)
 {
-	void print_alphabet(void);
 	int i;
 
-	for (i= 'a'; i <= 'z'; i++);
+	for (i= 'a'; i <= 'z'; i++)
 	{
-		putchar(i++);
+		_putchar(i++);
 	}
-	putchar('\n');
-	return(0);
+	_putchar('\n');
 }
