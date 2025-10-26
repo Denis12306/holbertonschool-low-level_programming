c645b6fcac587270999e92776f6aecf10921e986
Denis12306 <denis.gavaud@gmail.com>
2025-10-26 17:44:27 +0000

    JackBauer8-24

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * jack_bauer Welcome to his life
- * Description prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer print every minutes
+ * of the day of Jack Bauer from 00:00 to 23:59.
  * H is for the hours
  * M is for the minutes
  */
