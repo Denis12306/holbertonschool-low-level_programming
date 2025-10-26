d7df6089803018b837637e04542c81e6b081c229
Denis12306 <denis.gavaud@gmail.com>
2025-10-26 15:43:16 +0000

    SignNumber

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,9 +1,11 @@
 #include "main.h"
-/*
- * print_sign - prints the sign of a number.
- * @n : the number to verify
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: The number to check.
  *
- * Return: 0,1 or -1.
+ * Return: 1 and prints '+' if n is greater than zero,
+ * 0 and prints '0' if n is zero,
+ * -1 and prints '-' if n is less than zero.
  */
 int print_sign(int n)
 {
