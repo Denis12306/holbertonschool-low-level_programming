367c7ceb1e2eaa53b10655874f33de8e112f6f6c
Denis12306 <denis.gavaud@gmail.com>
2025-10-27 13:02:22 +0000

    PrintNumber

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,9 +3,9 @@
 /**
  * mul - for multiplies
  * Description: Write a function that multiplies two integers.
- * @a and @b are two integers
- * n is the result of the multiplication
- * Return n
+ * @a and @b are two integers to multiplied
+ * @n is the result of the multiplication
+ * Return n : Success
  */
 int mul(int a, int b)
 {
