9e615e50115a03eb4d6a462342f6757d50985345
Denis12306 <denis.gavaud@gmail.com>
2025-10-28 16:13:28 +0000

    PrintBlank

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,24 +7,19 @@
  */
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
-		_putchar('\n');
-	} else
+	_putchar('\n');
+	}
+	for (i = 0; i < n; i++)
 	{
-		int i;
-		int j;
-
-		for (i = 1; i <= n; i++)
-		{
-			_putchar(' ');
-
-			for (j = 1; j < i; j++)
-
-				_putchar('\\');
-		}
+	for (j = 0; j < i; j++)
+	{
+	_putchar(' ');
 	}
-		_putchar('\n');
+	_putchar('\\');
+	_putchar('\n');
 	}
-
 }
