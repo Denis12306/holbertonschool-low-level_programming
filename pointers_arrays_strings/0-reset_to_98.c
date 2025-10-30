8c365a45c33d3451eb9732269915f2c87ebfae18
Denis12306 <denis.gavaud@gmail.com>
2025-10-30 16:27:13 +0000

    SwappingAB

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 - Write a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98.
+ * @n is the value of the integer to change
  * Return: void
  */
 void reset_to_98(int *n)
