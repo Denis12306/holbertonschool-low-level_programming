c4c072be72e4731b14e8244aa2a5cc429a455019
Denis12306 <denis.gavaud@gmail.com>
2025-10-31 14:43:22 +0000

    OneOnTwo

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,11 @@ void puts2(char *str)
 	int len = 0;
 	int i;
 
-	while (len[str] != '\0')
+	while (str[len] != '\0')
 	{
 		len++;
 	}
-	for (i = 0; i <= len; i =+2)
+	for (i = 0; i < len; i +=2)
 	{
 		_putchar(str[i]);
 	}
