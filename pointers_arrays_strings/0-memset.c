4aa6ddf172eac62bd2b6ec2a6dd487b7b6640cb1
Denis12306 <denis.gavaud@gmail.com>
2025-11-03 12:43:18 +0000

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _memset - function that fills memory with a constant byte.
- * @b is te constant byte
+ * _memset a function that fills memory with a constant byte.
+ * @b is the constant byte
  * @n is the first byte
- * Return: Always 0
+ * @i is an integer input
+ * Return: @s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -14,6 +15,6 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
-
+	
 	return(s);
 }
