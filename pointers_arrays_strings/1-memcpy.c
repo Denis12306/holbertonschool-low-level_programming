d7fd1382803186d6f37356cb788d98df3abd37f4
Denis12306 <denis.gavaud@gmail.com>
2025-11-03 13:58:10 +0000

    Copy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 /**
  * _memcpy - a function that copies memory area
- *
- *
- *
+ * @src is the start area
+ * @dest is the end area
+ * @n is the number of bytes
  * Return: always 0
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
