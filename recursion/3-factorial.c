f05f286d778bafebfd2073173f05a87f433b5276
Denis12306 <denis.gavaud@gmail.com>
2025-11-05 08:47:59 +0000

    Factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * factorial - print the factorial of a number
+ * If n is lower than 0, the function should return -1 to indicate an error
+ * @n is an input interger
+ * Return: 1 or -1
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
