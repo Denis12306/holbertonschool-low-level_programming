fc07d8654cb698f4a203db5ba1d1c10f61073575
Denis12306 <denis.gavaud@gmail.com>
2025-11-12 12:42:54 +0000

    ArrayOfIntegers

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int *array_range(int min, int max)
+{
+	int *new_array;
+	int diff, i;
+
+	if (min > max)
+		return (NULL);
+
+	diff = max - min;
+	new_array = malloc((diff + 1) * sizeof(int));
+	if (new_array == NULL)
+		return (NULL);
+
+	for (i = 0; i <= diff; i++)
+		new_array[i] = min++;
+
+	return (new_array);
+}
