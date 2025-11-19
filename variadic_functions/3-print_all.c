3998346c61274a70820c87e107b96e31235356d2
Denis12306 <denis.gavaud@gmail.com>
2025-11-19 15:30:19 +0000

    PrintAll

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - a function that print anythings
+ * 
+ *
+ *
+ */
+print_all(const char * const format, ...)
+{
