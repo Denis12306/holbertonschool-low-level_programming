306e284da7604fa14ec3bc683111ce124ea20be6
Denis12306 <denis.gavaud@gmail.com>
2025-12-02 18:56:51 +0100

    Create1-list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -6,4 +6,15 @@
  * Return the number of element
  */
 size_t list_len(const list_t *h)
-{}
+{
+    int n = 0;
+
+    while (h)
+{
+    printf("%d\n", h->len);
+
+    else
+    return (-1);
+}
+return (n);
+}
