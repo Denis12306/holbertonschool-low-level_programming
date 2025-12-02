1cd13ed7c0d279245c85844641cec72702746d26
Denis12306 <denis.gavaud@gmail.com>
2025-12-02 19:43:16 +0100

    AddFiles

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * add_node - a function that add node to list_t
+ * @
+ * Return the adress of the new element
+ * or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+
+
+
+
+
+    
+}
