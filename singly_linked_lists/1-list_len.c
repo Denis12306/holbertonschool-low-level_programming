3cd3cc74e227e2fa98810b41a3284a702d92ce19
Denis12306 <denis.gavaud@gmail.com>
2025-12-02 15:13:15 +0100

    CreateFile

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len a function that returns the number of elements
+ * in a linked list_t list
+ * Return the number of element
+ */
+size_t list_len(const list_t *h)
+{}
