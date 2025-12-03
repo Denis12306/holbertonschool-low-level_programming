6ae7265426d64c3a4f6239cefe01d7257e9114f9
Denis12306 <denis.gavaud@gmail.com>
2025-12-03 15:42:41 +0100

    Modif

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,14 +7,12 @@
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+    list_t *temp;
 
-	temp = head;
-	while (head)
-	{
-		temp = head;
-		head = head->next;
-		free(temp->str);
-		free(temp);
-	}
+    while (head != NULL)
+    {
+        temp = head->next;
+        free(head->str);
+        free(head);
+        head = temp;
 }
